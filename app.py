6c2c5a43f741875206d2c2827137f7985d1e3ff9
rombuskii <eruba.e@outlook.com>
2024-08-02 09:44:19 -0400

    Added decision tree

--- app.py
+++ app.py
@@ -17,8 +17,11 @@ cors = CORS(app, resources={
 })
 
 # Load the trained model and encoder
-model = joblib.load('logistic_regression_model.pkl')
-encoder = joblib.load('encoder.pkl')
+prob_encoder = joblib.load('encoder.pkl')
+prob_model = joblib.load('logistic_regression_model.pkl')
+date_encoder =  joblib.load('encoder_dt.pkl')
+date_model = joblib.load('decision_tree_model.pkl')
+
 
 def preprocess_data(sku, quantity, date_placed, actual_shipment_date):
     # Convert inputs to DataFrame
@@ -29,11 +32,31 @@ def preprocess_data(sku, quantity, date_placed, actual_shipment_date):
     })
     
     # One-hot encode the 'sku' column
-    sku_encoded = encoder.transform(data[['sku']])
-    sku_encoded_df = pd.DataFrame(sku_encoded.toarray(), columns=encoder.get_feature_names_out(['sku']))
+    sku_encoded = prob_encoder.transform(data[['sku']])
+    sku_encoded_df = pd.DataFrame(sku_encoded.toarray(), columns=prob_encoder.get_feature_names_out(['sku']))
     
     # Combine encoded features with the original features
     data_final = pd.concat([data.drop(columns=['sku']), sku_encoded_df], axis=1)
+    
+    return data_final
+
+def preprocess_getDate(sku, quantity, actual_shipment_date):
+    data = pd.DataFrame({
+        'sku': [sku],
+        'quantity': [quantity],
+        'actualShipmentDate': [actual_shipment_date]
+    })
+    print(data)
+
+    # One-hot encode 'sku'
+    df_encoded = date_encoder.transform(data[['sku']])
+    df_encoded_df = pd.DataFrame(df_encoded.toarray(), columns=date_encoder.get_feature_names_out(['sku']))
+
+    # Combine the encoded features with other features
+    data_final = pd.concat([data.drop(columns=['sku']).reset_index(drop=True), df_encoded_df.reset_index(drop=True)], axis=1)
+
+    # Convert 'actualShipmentDate' to numeric (UNIX timestamp)
+    data_final['actualShipmentDate'] = pd.to_datetime(data_final['actualShipmentDate']).astype(int) / 10**9
     
     return data_final
 
@@ -41,8 +64,35 @@ def preprocess_data(sku, quantity, date_placed, actual_shipment_date):
 def hello():
     return "Clarios Logistic Regression Model"
 
+
+@app.route('/getDate', methods=['POST'])
+def getDate():
+    # Get data from request
+    data = request.json
+    sku = data.get('sku')
+    quantity = data.get('quantity')
+    actual_shipment_date = data.get('actualShipmentDate')
+
+    if not sku:
+        sku = 'HE-FE'
+    
+    if not quantity:
+        quantity = 151218
+
+    # Preprocess the data
+    processed_data = preprocess_getDate(sku, quantity, actual_shipment_date)
+    
+    # Make prediction and get probability
+    days_to_ship = date_model.predict(processed_data)
+
+    prediction = pd.to_datetime(actual_shipment_date) - pd.Timedelta(days=days_to_ship[0])
+    
+    return jsonify({'days_to_ship': prediction.strftime('%m/%d/%Y')})
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
+    model = joblib.load('logistic_regression_model.pkl')
     # Get data from request
     data = request.json
     sku = data.get('sku')
@@ -60,7 +110,7 @@ def predict():
     processed_data = preprocess_data(sku, quantity, date_placed, actual_shipment_date)
     
     # Make prediction and get probability
-    probability = model.predict_proba(processed_data)[0][1]  # Probability of being 'Completed'
+    probability = prob_model.predict_proba(processed_data)[0][1]  # Probability of being 'Completed'
     
     # Convert probability to percentage
     probability_percentage = probability * 100
